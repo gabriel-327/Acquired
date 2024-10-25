b4dd1ba5eb5d454cbd730f7cae8e66e4b40f1894
Gabriel Frank <gfrank1@ufl.edu>
2024-10-25 12:41:57 -0400

    Basic backend - MongoDB and postman working, create, delete, edit, and view all for products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './button';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Item> </Item>
       </header>
     </div>
   );
---
+++ src/button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyButton() {
+    return (
+      <button>Click on me</button>
+    );
+  }
+
+  export default function Item() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+
