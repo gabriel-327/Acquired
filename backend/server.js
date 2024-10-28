59e8127ceb66590a0b0f3c4072f886e83511da6f
dsasteesha <sdsa@ufl.edu>
2024-10-28 16:06:32 -0400

    testing server changes

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import { connectDB } from "./config/db.js";
-
-// // added to connect backend and frontend
-// import cors from 'cors';
-// app.use(cors());
-
-
-// import productRoutes from "./routes/product.route.js";
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(express.json()); // allows us to accpet JSON data in the req.body
-
-// app.use("/api/products", productRoutes);
-
-// app.listen(PORT, () => {
-//     connectDB();
-//     console.log("Server started at http://localhost:" + PORT);
-// });
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
